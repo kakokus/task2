b8409794a51e7385ea168be4c1b510d80ae56bf8
ASmolyachkov <asmaliachkou@gmail.com>
2021-12-28 18:20:06 +0300

    task2

--- task2/ViewController.swift
+++ task2/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeuereusableCellWithIdentifier(cellReuseIdentifier) as UITableViewCell!
+        
+        cell.textLabel?.text = countries[indexPath.row]
+        
+        return cell
+    }
+    
 
+    let cellReuseIdentifier = "cell"
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var countries: [String] = []
+        
+        for code in NSLocale.isoCountryCodes {
+            let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
+            let name = NSLocale(localeIdentifier: "en_UK").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Not found: \(code)"
+            countries.append(name)
+        }
+        
+        print(countries)
+        
+        tableView.frame = CGRect(x: 0, y: 50, width: 320, height: 200)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseidentifier: cellReuseIdentifier)
+        
+        self.view.addSubview(tableView)
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
 
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("you tapped \(indexPath.row)")
+    }
+    
 
+    
+    
 }
 
